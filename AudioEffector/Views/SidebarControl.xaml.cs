f5fdc44abc7fe4f31915a42ca3245bec339f30ce
taketonnbo <t.nishiuchi2000@gmail.com>
2026-08-28 22:47:22 +0900

    feat: ✨ #67 サイドバーの基本枠(UI)およびホバー展開アニメーションを実装

---
+++ AudioEffector/Views/SidebarControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AudioEffector.Views
+{
+    public partial class SidebarControl : UserControl
+    {
+        public SidebarControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
