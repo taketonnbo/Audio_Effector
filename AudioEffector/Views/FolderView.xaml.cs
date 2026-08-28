edbe272e0d0b50b2e09cdc9ea18569d1eaf0322b
taketonnbo <t.nishiuchi2000@gmail.com>
2026-08-29 00:20:23 +0900

    [Fix] Add missing code-behind files for extracted UserControls

---
+++ AudioEffector/Views/FolderView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AudioEffector.Views
+{
+    public partial class FolderView : UserControl
+    {
+        public FolderView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AudioEffector/Views/PlaylistTracksView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AudioEffector.Views
+{
+    public partial class PlaylistTracksView : UserControl
+    {
+        public PlaylistTracksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
