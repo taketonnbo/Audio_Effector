068532021f8b186acd459a3f463da4758058dde5
taketonnbo <t.nishiuchi2000@gmail.com>
2026-09-01 20:07:00 +0900

    feat: ✨ #122 スペクトラムアナライザ連動リサイズとトラックリスト表示を改善する

---
+++ AudioEffector/IndexToZebraBackgroundConverter.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace AudioEffector
+{
+    /// <summary>
+    /// リストのAlternationIndexに基づいてゼブラストライプ用の背景ブラシ（奇数行に微細な明るい背景）を返すコンバーター。
+    /// </summary>
+    public class IndexToZebraBackgroundConverter : IValueConverter
+    {
+        private static readonly SolidColorBrush EvenBrush = Brushes.Transparent;
+        private static readonly SolidColorBrush OddBrush = new SolidColorBrush(Color.FromArgb(12, 255, 255, 255)); // 約5%透明度の白
+
+        static IndexToZebraBackgroundConverter()
+        {
+            OddBrush.Freeze();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                return (index % 2 == 1) ? OddBrush : EvenBrush;
+            }
+            return EvenBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
