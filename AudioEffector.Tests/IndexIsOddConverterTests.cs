fbe1cdfcee023e9d16c944139a87feaa912975e1
taketonnbo <t.nishiuchi2000@gmail.com>
2026-09-01 21:33:41 +0900

    test: 🧪 #122 IndexIsOddConverterの単体テストを追加する

---
+++ AudioEffector.Tests/IndexIsOddConverterTests.cs
@@ -0,0 +1,31 @@
+using AudioEffector;
+using System.Globalization;
+using Xunit;
+
+namespace AudioEffector.Tests
+{
+    public class IndexIsOddConverterTests
+    {
+        private readonly IndexIsOddConverter _converter = new IndexIsOddConverter();
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(1, true)]
+        [InlineData(2, false)]
+        [InlineData(3, true)]
+        [InlineData(10, false)]
+        [InlineData(99, true)]
+        public void Convert_ReturnsCorrectOddState(int index, bool expected)
+        {
+            var result = _converter.Convert(index, typeof(bool), null, CultureInfo.InvariantCulture);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Convert_NonInt_ReturnsFalse()
+        {
+            var result = _converter.Convert("not-an-int", typeof(bool), null, CultureInfo.InvariantCulture);
+            Assert.False((bool)result);
+        }
+    }
+}
